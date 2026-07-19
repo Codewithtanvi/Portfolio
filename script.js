74043aa653c8ab7bd939104203f095bc74e6358e
Tanvi <taniengg6388@gmail.com>
2026-07-20 01:19:39 +0530

    Projects

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+function openModal(title, image, description, tech, github, live){
 
+    document.getElementById("projectModal").style.display = "block";
+
+    document.getElementById("modalTitle").innerHTML = title;
+    document.getElementById("modalImg").src = image;
+    document.getElementById("modalDesc").innerHTML = description;
+    document.getElementById("modalTech").innerHTML = tech;
+    document.getElementById("githubBtn").href = github;
+    document.getElementById("liveBtn").href = live;
+}
+
+function closeModal(){
+    document.getElementById("projectModal").style.display = "none";
+}
+ 
+window.onclick=function(e){
+
+    const modal=document.getElementById("projectModal");
+
+        if(e.target==modal){
+
+            modal.style.display="none";
+}
+
+}
+
+document.getElementById("projectModal").style.display = "block";
